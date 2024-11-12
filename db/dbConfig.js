64fef0cf011a8c154cfecb5fb146dca2c63f9bd5
deepak_chauhan <deepak.chauhan@carrum.co.in>
2024-11-12 18:48:48 +0530

    chore: Update MongoDB connection URL in dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 const DB_URI =
   "mongodb+srv://deepak:z9W6wWDVRqfalm2Z@cluster0.amyr9.mongodb.net/";
+const url = "mongodb+srv://kapil:testingkatto@cluster0.lpe9kln.mongodb.net/";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB_URI, {
+    await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
