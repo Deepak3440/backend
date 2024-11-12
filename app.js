54aa51d16865069b94607ad3793a0eced7c8615d
deepak_chauhan <deepak.chauhan@carrum.co.in>
2024-11-12 19:03:12 +0530

    chore: Update MongoDB connection URL and add morgan logging middleware

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const connectDB = require("./db/dbConfig");
 const authRoutes = require("./routes/authRoutes");
+const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
-
+app.use(morgan("dev"));
 connectDB();
 
 app.use("/api/auth", authRoutes); // Set up the auth routes
