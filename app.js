c514814f0c0ca10069a1bda37c517ac0765da8fc
deepak_chauhan <deepak.chauhan@carrum.co.in>
2025-01-29 12:26:10 +0530

    chore: Remove morgan logging middleware from app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 connectDB();
 
 app.use("/api/auth", authRoutes); // Set up the auth routes
