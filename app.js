a4f7522f4e20b0561d36c196c6fb7a27599ea36f
deepak_chauhan <deepak.chauhan@carrum.co.in>
2025-01-29 13:18:55 +0530

    add console

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ connectDB();
 
 app.use("/api/auth", authRoutes); // Set up the auth routes
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
